46c6493842ddf073326bec329e73a009a4b8f7c3
Obama00 <soap2thedoam@yahoo.com>
2022-08-31 15:24:59 -0400

    8/31 lab

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -2,26 +2,50 @@
 
 namespace Zork
 {
+
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
 
-            string inputString = Console.ReadLine().Trim();
-            inputString = inputString.ToUpper();
-            if (inputString == "QUIT")
-            {
-                Console.WriteLine("Thank you for playing.");
-            }
-            else if (inputString == "LOOK") 
-            {
-                Console.WriteLine("This is an open field west of a white house, with a boarded front door. \nA rubber mat saying 'Welcome to Zork!' lies by the door.");
-            }
-            else
-            {
-                Console.WriteLine("Unrecognized command.");
-            }
+            string inputString = Console.ReadLine();
+            Commands command = ToCommand(Console.ReadLine().Trim());
+            Console.WriteLine(command);
+
         }
+
+        private static Commands ToCommand(string commandString)
+        {
+            Commands command;
+            switch (commandString)
+            {
+                case "QUIT":
+                    command = Commands.QUIT;
+                    break;
+
+                case "LOOK":
+                    command = Commands.LOOK;
+                    break;
+                case "NORTH":
+                    command = Commands.NORTH;
+                    break;
+                case "SOUTH":
+                    command = Commands.SOUTH;
+                    break;
+                case "EAST":
+                    command = Commands.EAST;
+                    break;
+                case "WEST":
+                    command = Commands.WEST;
+                    break;
+                default:
+                    command = Commands.UNKNOWN;
+                    break;
+            };
+            return command;
+
+        }
+
     }
 }
