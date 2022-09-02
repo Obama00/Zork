bd91579c9c4285199fe9d76af5e6d92dbf6fbea7
Kevin Perez <kperez49@knights.ecf.edu>
2022-09-02 17:12:13 -0400

    Zork 1.2
    Finding direction using Enums

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -10,42 +10,13 @@ namespace Zork
             Console.WriteLine("Welcome to Zork!");
 
             string inputString = Console.ReadLine();
-            Commands command = ToCommand(Console.ReadLine().Trim());
+            Commands command = ToCommand(inputString.Trim().ToUpper());
             Console.WriteLine(command);
 
         }
 
-        private static Commands ToCommand(string commandString)
-        {
-            Commands command;
-            switch (commandString)
-            {
-                case "QUIT":
-                    command = Commands.QUIT;
-                    break;
-
-                case "LOOK":
-                    command = Commands.LOOK;
-                    break;
-                case "NORTH":
-                    command = Commands.NORTH;
-                    break;
-                case "SOUTH":
-                    command = Commands.SOUTH;
-                    break;
-                case "EAST":
-                    command = Commands.EAST;
-                    break;
-                case "WEST":
-                    command = Commands.WEST;
-                    break;
-                default:
-                    command = Commands.UNKNOWN;
-                    break;
-            };
-            return command;
-
-        }
+        private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
+       
 
     }
 }
