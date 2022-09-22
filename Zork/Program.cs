839fe7fd6302eda41d1db2946f775cef4a558be6
Kevin Perez <kperez49@knights.ecf.edu>
2022-09-22 19:14:10 -0400

    Revert "1st attempt to revert to 3.1"
    This reverts commit 160b44c6c78cfbdb53b397081f19693112fa4b62.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -6,7 +6,7 @@ namespace Zork
 
    internal class Program
     {
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -15,17 +15,10 @@ namespace Zork
         }
         static void Main(string[] args)
         {
-            Room westOfHouse = new Room("West Of House");
-            Room southOfHouse = new Room("South Of House");
-
-            List<Room> rooms = null;
-            rooms.Add(westOfHouse);
-            rooms.Add(southOfHouse);
-
-            foreach (Room room in )
-            
             Console.WriteLine("Welcome to Zork!");
+            InitializeRoomDescriptions();
 
+            Room previousRoom = null;
             Commands command = Commands.UNKNOWN;
             bool isRunning = true;
             while (isRunning)
@@ -51,7 +44,7 @@ namespace Zork
                             break;
 
                         case Commands.LOOK:
-                            outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                           Console.WriteLine(CurrentRoom.Description);
                             break;
 
                         case Commands.NORTH:
@@ -72,7 +65,7 @@ namespace Zork
                             outputString = "Unknown command.";
                             break;
                     };
-                    Console.WriteLine(outputString);
+                
                 }
             }
         }
@@ -109,10 +102,10 @@ namespace Zork
 
         private static bool IsDirection(Commands command) => Directions.Contains(command);
 
-        private static readonly string[,] _rooms = {
-            {"Rocky Trail", "South of House", "Canyon View"},
-        {"Forest", "West of House", "Behind House" },
-        {"Dense Woods", "North of House", "Clearing" } };
+        private static readonly Room[,] _rooms = {
+        {new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View")},
+        {new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+        {new Room("Dense Woods"), new Room("North of House"), new Room("Clearing") } };
        
 
         private static readonly List<Commands> Directions = new List<Commands>
@@ -123,6 +116,27 @@ namespace Zork
         Commands.WEST,
     };
         private static (int Row, int Column) Location = (1, 1);
+
+        private static void InitializeRoomDescriptions()
+        {
+            var roomMap = new Dictionary<string, Room>();
+            foreach (Room room in _rooms)
+            {
+                roomMap[room.Name] = room;
+            }
+
+            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
+            roomMap["South of House"].Description = "You are facing the south side of a white house.  There is no door here, and all the windows are barred.";
+            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
+
+            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
+            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
+            roomMap["Behind House"].Description = "You are behind the white house.  In one corner of the house there is a small window which is slightly ajar.";
+
+            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around.  To the east, there appears to be sunlight.";
+            roomMap["North of House"].Description = "You are facing the north side of a white house.  There is no door here, and all the windows are barred.";
+            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+        }
     }
    
 }
