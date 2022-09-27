f4f148ed26770bcd6b77de4283280eb9fce88adf
Kevin Perez <kperez49@knights.ecf.edu>
2022-09-27 19:10:12 -0400

    Zork 4.1
    set up room descriptions to read from a different file.

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Zork
 {
 
+
     internal class Program
     {
+        private enum CommandLineArguments
+        {
+            RoomsFilename = 0
+        }
         private static Room CurrentRoom
         {
             get
@@ -15,8 +21,10 @@ namespace Zork
         }
         static void Main(string[] args)
         {
+            const string defaultRoomsFilename = "Rooms.txt";
+            string roomsFilename = (args.Length > 0 ? args[(int)CommandLineArguments.RoomsFilename] : defaultRoomsFilename);
             Console.WriteLine("Welcome to Zork!");
-            InitializeRoomDescriptions();
+            InitializeRoomDescriptions(roomsFilename);
             Room previousRoom = null;
             Commands command = Commands.UNKNOWN;
             bool isRunning = true;
@@ -48,11 +56,12 @@ namespace Zork
                         case Commands.WEST:
                             if (Move(command))
                             {
-                                outputString = "The way is shut!";
+                                outputString = $"You moved {command}";
                             }
                             else
                             {
-                                outputString = $"You moved {command}";
+                                outputString = "The way is shut!";
+                                
                             }
                             break;
                         default:
@@ -106,26 +115,41 @@ namespace Zork
     };
         private static (int Row, int Column) Location = (1, 1);
 
-        private static void InitializeRoomDescriptions()
+        private enum Fields
         {
-            var roomMap = new Dictionary<string, Room>();
-            foreach (Room room in _rooms)
+            Name = 0,
+            Description
+        }
+
+        private static void InitializeRoomDescriptions(string roomsFilename)
+        {
+            const string fieldDelimiter = "##";
+            const int expectedFieldCount = 2;
+
+            string[] lines = File.ReadAllLines(roomsFilename);
+            foreach (string line in lines)
             {
-                roomMap[room.Name] = room;
+                string[] fields = line.Split(fieldDelimiter);
+                if (fields.Length != expectedFieldCount)
+                {
+                    throw new InvalidDataException("Invalid record.");
+                }
+                string name = fields[(int)Fields.Name];
+                string description = fields[(int)Fields.Description];
+                RoomMap[name].Description = description;
             }
 
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house.  There is no door here, and all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Canyon on its south wall.";
-
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house.  In one corner of the house there is a small window which is slightly ajar.";
-
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around.  To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house.  There is no door here, and all the windows are barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+           
+        }
+        private static readonly Dictionary<string, Room> RoomMap;
+        static Program()
+        {
+            RoomMap = new Dictionary<string, Room>();
+            foreach (Room room in _rooms)
+            {
+                RoomMap[room.Name] = room;
+            }
         }
     }
-
+  
 }
