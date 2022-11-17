7ff906a9afec28772bd6b5c23ae4a84c70a81b6d
Obama00 <112011874+Obama00@users.noreply.github.com>
2022-11-17 18:21:26 -0500

    11/17
    set up scroll view and started getting text to display on interface

--- Zork.Unity/Assets/Scripts/GameManager.cs
+++ Zork.Unity/Assets/Scripts/GameManager.cs
@@ -14,16 +14,48 @@ public class GameManager : MonoBehaviour
     private TextMeshProUGUI MovesText;
 
     [SerializeField]
-    private UnityInputService Input;
+    private UnityInputService InputService;
     [SerializeField]
-    private UnityOutputService Output;
+    private UnityOutputService OutputService;
 
     private void Awake()
     {
         TextAsset gameJson = Resources.Load<TextAsset>("GameJson");
         _game = JsonConvert.DeserializeObject<Game>(gameJson.text);
-       _game.Run(Input, Output);
-       
+        _game.Player.LocationChanged += Player_LocationChanged;
+       _game.Run(InputService, OutputService);
+        
+    }
+
+    private void Player_LocationChanged(object sender, Room location)
+    {
+        LocationText.text = location.Name;
+    }
+
+    private void Start()
+    {
+        InputService.SetFocus();
+        LocationText.text = _game.Player.CurrentRoom.Name;
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            InputService.ProcessInput();
+            InputService.SetFocus();
+        }
+
+        if (_game.IsRunning == false)
+        {
+#if UNITY_EDITOR
+            // Application.Quit() does not work in editor so
+            // UnityEditor.EditorApplication.isPlaying need to be set to false to end the game
+            UnityEditor.EditorApplication.isPlaying = false;
+#else
+            Application.Quit();
+#endif
+        }
     }
     private Game _game;
 }
